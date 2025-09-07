749ff94685c33ef8030bfd956f0c2463aa68205d
Mame60 <amemama19@gmail.com>
2025-09-07 01:41:16 +0000

    updateFi

--- app.py
+++ app.py
@@ -33,6 +33,6 @@ def recherche():
         results = []
     
     return jsonify(results)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Railway donne le PORT automatiquement
+    app.run(host="0.0.0.0", port=port)
